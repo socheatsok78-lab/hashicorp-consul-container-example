d08aa2e229e25b549fba22d10181c6626aa91fde
Socheat Sok <alex@socheat.net>
2020-04-11 18:29:30 +0700

    init docker-compose.yml for consul with nginx

---
+++ bootstrap/services/web/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+echo "Starting Consul agent..."
+consul agent \
+    -retry-join ${CONSUL_HTTP_ADDR} \
+    -client 0.0.0.0 \
+    -enable-script-checks \
+    -config-dir=/etc/consul.d \
+    -data-dir=/tmp/consul &
+
+exec nginx -g 'daemon off;'
